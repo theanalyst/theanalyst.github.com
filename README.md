205669beda5bfd921264297a263121ebdecdf7a2
Abhishek L <abhishekl.2006@gmail.com>
2012-06-11 18:05:10 +0530

    Added Readme

---
+++ README.md
@@ -0,0 +1,1 @@
+An attempt at a blog at GitHub Pages
---
+++ pelican-conf.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+AUTHOR = u'abhishekl'
+SITENAME = u"Hyperbolic Monologues"
+SITEURL = 'http://theanalyst.github.com'
+TIMEZONE = "Asia/Kolkata"
+DISPLAY_PAGES_ON_MENU = True
+ARTIClE_DIR = ('rants')
+
+GITHUB_URL = 'http://github.com/theanalyst/'
+DISQUS_SITENAME = "hyperbolicmonologues"
+PDF_GENERATOR = False
+REVERSE_CATEGORY_ORDER = True
+LOCALE = ""
+DEFAULT_PAGINATION = 4
+
+FEED_RSS = 'feeds/all.rss.xml'
+CATEGORY_FEED_RSS = 'feeds/%s.rss.xml'
+
+SOCIAL = (('twitter', 'http://twitter.com/abhishekl'),
+          ('github', 'http://github.com/theanalyst'),)
+
+# global metadata to all the contents
+DEFAULT_METADATA = (('yeah', 'it is'),)
+
+# static paths will be copied under the same name
+STATIC_PATHS = ["pictures", ]
+
+# A list of files to copy from the source to the destination
+#FILES_TO_COPY = (('extra/robots.txt', 'robots.txt'),)
+
+
